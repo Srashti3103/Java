ca954fa5e94a1a93dbeaef1c6337cff855ddfb4e
Srashti3103 <srashtipatel31@gmail.com>
2026-06-14 23:10:57 +0530

    To explain arithmetic operators

---
+++ src/ArithmeticOperator.java
@@ -0,0 +1,20 @@
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b=5;
+		System.out.println(a+b);//Addition:15
+		System.out.println(a-b);//Subtraction:5
+		System.out.println(a*b);//Multiplication:50
+		System.out.println(a/b);//Division:2
+		System.out.println(a%b);//Modulus(remainder):0
+		a++;//Post increment a becomes 11
+		System.out.println(a);//11
+		++b;//Pre increment b  becomes 6		
+		System.out.println(b);//6
+		
+		
+	}
+
+}
