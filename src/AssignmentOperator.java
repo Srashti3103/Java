622cd42ce1f4f055d78fd4a4a5412e1f215d058a
Srashti3103 <srashtipatel31@gmail.com>
2026-06-14 23:11:28 +0530

    To explain assignment operators

---
+++ src/AssignmentOperator.java
@@ -0,0 +1,17 @@
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int c= 10;
+		c+=5; //Equivalent to c = c+5
+		System.out.println(c);//15
+		c-=3;//Equivalent to c=c-3
+		System.out.println(c);//12
+		c*=2;//Equivalent to c=c*2
+		System.out.println(c);//24
+		c/=4;//Equivalent to c=c/4
+		System.out.println(c);//6
+		c%=5;//Equivalent to c =c % 5
+		System.out.println(c);//1
+	}
+
+}
