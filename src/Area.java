a1114bd17f84ccc9925dddefd1be22a9f2247069
Srashti3103 <srashtipatel31@gmail.com>
2026-06-17 23:16:20 +0530

    Area

---
+++ src/Area.java
@@ -0,0 +1,12 @@
+
+
+public interface Area {
+	public final static float pi = 3.14f;
+	float x = 3.14f;
+	//var ---> by default ---> public , final , static  ---> optional
+	
+	float compute (float a, float b);
+	//bydefault method ---> public , abstract 
+	
+	//public abstract float  compute1(float a , float b);
+}
