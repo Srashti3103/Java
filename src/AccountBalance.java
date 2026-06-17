3e7c033cc883ceba0009fbabafe544216664316f
Srashti3103 <srashtipatel31@gmail.com>
2026-06-17 23:14:38 +0530

    Account Package

---
+++ src/AccountBalance.java
@@ -0,0 +1,11 @@
+//accessing package 
+import account.Balance;
+public class AccountBalance {
+
+	public static void main(String[] args) {
+		Balance obj = new Balance("ss", 10.50);
+
+		obj.show(); 
+	}
+
+}
