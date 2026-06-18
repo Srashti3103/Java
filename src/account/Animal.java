0b927313dbbd6c2b5a9ab1e77662e54095bfe41b
Srashti3103 <srashtipatel31@gmail.com>
2026-06-18 09:06:48 +0530

    Animal

---
+++ src/account/Animal.java
@@ -0,0 +1,13 @@
+package account;
+
+public class Animal {
+	protected Animal() {
+		System.out.println("Animal constructor called.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
