6e41d19c9972dd7e22e3a48b55f9c51377b661e2
Srashti3103 <srashtipatel31@gmail.com>
2026-06-23 08:50:06 +0530

    CheckEven

---
+++ src/CheckEven.java
@@ -0,0 +1,24 @@
+
+public class CheckEven {
+
+	public static void main(String[] args) {
+		int num = 2486;
+		boolean allEven = true;
+		while(num !=0) {
+			int digit = num % 10;
+			if(digit % 2 != 0) {
+				allEven = false;
+				break;
+			}
+			num = num /10;
+			
+		}
+		if(allEven) {
+			System.out.println("All digits are even.");
+		}else {
+			System.out.println("Not all digits are even.");
+		}
+
+	}
+
+}
