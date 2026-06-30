9b2f8ef9e1a4468db2c535dd629ae033aeb382f3
Srashti3103 <srashtipatel31@gmail.com>
2026-06-30 23:22:16 +0530

    Conditional operator

---
+++ src/ConditionalOperator.java
@@ -0,0 +1,16 @@
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		/*
+		 * used to evaluate boolean expressions
+		 * operator : && , ||
+		 */
+		int h =10;
+		int i=20;
+		if(h>5 && i<30) {
+			System.out.println("Both conditions are true ");
+		}
+	}
+
+}
