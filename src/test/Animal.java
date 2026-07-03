ff179679d25c9c2a3e0b0c11b489cd08ea23ece8
Srashti3103 <srashtipatel31@gmail.com>
2026-07-03 22:15:07 +0530

    Animal

---
+++ src/test/Animal.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Animal {
+	protected String name;
+	protected void eat() {
+		System.out.println(name+"is eating.");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
