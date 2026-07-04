1cd2b6e7150f975c5e81a3b94b9343bb41534672
Srashti3103 <srashtipatel31@gmail.com>
2026-07-04 23:20:13 +0530

    Convert number system

---
+++ src/ConvertNumberSys.java
@@ -0,0 +1,14 @@
+
+public class ConvertNumberSys {
+
+	public static void main(String[] args) {
+		int decimal = 25;
+		int octal = 031;
+		int hexadecimal = 0x1A;
+		System.out.println("Decimal:"+decimal);
+		System.out.println("Octal:"+octal);
+		System.out.println("Hexadecimal:"+hexadecimal);
+
+	}
+
+}
