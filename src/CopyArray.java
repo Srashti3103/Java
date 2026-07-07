823e165e4503a5510691ecf3a3c705bd5d96de63
Srashti3103 <srashtipatel31@gmail.com>
2026-07-07 18:11:39 +0530

    copy array

---
+++ src/CopyArray.java
@@ -0,0 +1,17 @@
+//Copying arrays 
+/*
+ * Syntax --> 
+ * public static void arraycopy
+ * (Object src, int srcPos,Object dest , int destPos, int length)
+ */
+public class CopyArray {
+
+	public static void main(String[] args) {
+		char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a', 't', 'e', 'd' };
+		char[] copyTo = new char[7];
+		System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+		System.out.println(new String(copyTo));
+
+	}
+
+}
