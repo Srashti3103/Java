f5cf7d04668e290b71dc5ae4c429e56cbcec2f04
Srashti3103 <srashtipatel31@gmail.com>
2026-07-15 23:46:30 +0530

    Find number

---
+++ src/FindNumber.java
@@ -0,0 +1,19 @@
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int number=2;
+		switch(number) {
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		default:
+			System.out.println("No Pne or Two");
+		}
+
+	}
+
+}
