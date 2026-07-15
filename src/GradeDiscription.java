5ac5a7ae4f3ddb12bfd49de6c2dd79b1a0f8d4f6
Srashti3103 <srashtipatel31@gmail.com>
2026-07-15 20:53:12 +0530

    grade discription

---
+++ src/GradeDiscription.java
@@ -0,0 +1,28 @@
+
+public class GradeDiscription {
+
+	public static void main(String[] args) {
+		char grade = 'B';
+		switch (grade) {
+		case 'A':
+			System.out.println("Excellent!");
+			break;
+		case 'B':
+			System.out.println("Well done");
+			break;
+		case 'C':
+			System.out.println("Good");
+			break;
+		case 'D':
+			System.out.println("Passed");
+			break;
+		case 'F':
+			System.out.println("Try again");
+			break;
+		default:
+			System.out.println("Invalid grade");
+		}
+
+	}
+
+}
