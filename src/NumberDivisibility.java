c19c5e7f770f46e21ae3637400c75687e0e99bd8
Srashti3103 <srashtipatel31@gmail.com>
2026-07-25 22:31:22 +0530

    number divisibility

---
+++ src/NumberDivisibility.java
@@ -0,0 +1,14 @@
+
+public class NumberDivisibility {
+
+	public static void main(String[] args) {
+		int number = 30;
+		if(number % 3 == 0 && number % 5 == 0) {
+			System.out.println("Divisible by 3 and 5 ");
+		}else {
+			System.out.println("Not Divisible by 3 and 5");
+		}
+
+	}
+
+}
