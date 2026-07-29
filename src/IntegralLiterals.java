7995bb67768bf72ba3153bbd0ce4bdbb880a7b78
Srashti3103 <srashtipatel31@gmail.com>
2026-07-29 17:53:12 +0530

    integral literal

---
+++ src/IntegralLiterals.java
@@ -0,0 +1,18 @@
+
+public class IntegralLiterals {
+
+	public static void main(String[] args) {
+		//58
+		int a1 = 58;// decimal
+		int a2 = 0b101;//bin-->dec-->5
+		int a3 = 0123;//oct --> dec
+		int a4 = 0xabc;//hex1 --> dec
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		//int a5 = 0786;//oct---> 0 to 7 base 8
+		// int  a5  = 0xbeer; // hexa --> 0 to f base 16
+	}
+
+}
