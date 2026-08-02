4d30f3db6b989f1bb5009bb51f7e0200dc69cdf9
Srashti3103 <srashtipatel31@gmail.com>
2026-08-03 00:27:41 +0530

    local variable

---
+++ src/LocalVariable.java
@@ -0,0 +1,13 @@
+
+public class LocalVariable {
+	void display() {
+		int localVar = 10;//Local variable
+		System.out.println(localVar);
+	}
+
+	public static void main(String[] args) {
+		LocalVariable e1 = new LocalVariable();
+		e1.display();
+	}
+
+}
