12256498d6e1775344d07ee9051294a072984fbf
Srashti3103 <srashtipatel31@gmail.com>
2026-08-18 19:32:55 +0530

    insctanced operator

---
+++ src/InstanceofOperator.java
@@ -0,0 +1,15 @@
+
+public class InstanceofOperator {
+
+	public static void main(String[] args) {
+		/*
+		 * Used to check if an instance of a specific class of interface 
+		 * Operator : instanceof
+		 * 
+		 */
+		String str="Hello";
+		System.out.println(str instanceof String );//true
+
+	}
+
+}
