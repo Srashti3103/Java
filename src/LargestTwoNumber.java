5d69c97f7d7f1b5aa4cbb8a053a753787ecc6d98
Srashti3103 <srashtipatel31@gmail.com>
2026-08-19 16:07:03 +0530

    largest two numbewr

---
+++ src/LargestTwoNumber.java
@@ -0,0 +1,15 @@
+
+public class LargestTwoNumber {
+
+	public static void main(String[] args) {
+		int a=10, b=20;
+		if(a>b) {
+			System.out.println(a+" is larger.");
+		}else if(a<b) {
+			System.out.println(b+" is larger.");
+		}else {
+			System.out.println("Both are equal.");
+		}
+	}
+
+}
