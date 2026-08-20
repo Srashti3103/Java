5be7758d7dbccadef80bb223c0231cc511f7b235
Srashti3103 <srashtipatel31@gmail.com>
2026-08-20 20:09:24 +0530

    laargest three number

---
+++ src/LargestThreeNumber.java
@@ -0,0 +1,16 @@
+
+public class LargestThreeNumber {
+
+	public static void main(String[] args) {
+		int a = 15,b = 25,c = 20;
+		if(a>=b && a>=c) {
+			System.out.println("Largest:"+a);
+		}else if(b>=a && b>=c) {
+			System.out.println("Largest:"+b);
+		}else {
+			System.out.println("Largest:"+c);
+		}
+
+	}
+
+}
