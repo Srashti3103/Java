32bab112f35cce356e4d15f043724d2eeeb0bbc4
Srashti3103 <srashtipatel31@gmail.com>
2026-08-21 21:40:53 +0530

    logical operator

---
+++ src/LogicalOperator.java
@@ -0,0 +1,13 @@
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		boolean p=true;
+		boolean q=false;
+		System.out.println(p&&q);//logical AND :false
+		System.out.println(p||q);//logical OR :true
+		System.out.println(!p);//logical NOT :false
+
+	}
+
+}
