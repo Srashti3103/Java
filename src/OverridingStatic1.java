8149dcddc610a01f42945c4faac5705f0d944972
Srashti3103 <srashtipatel31@gmail.com>
2026-08-28 23:25:25 +0530

    overriding static

---
+++ src/OverridingStatic1.java
@@ -0,0 +1,17 @@
+//class p1{
+//	public static void goHome() {
+//		System.out.println("By Bus");
+//	}
+//}
+//public class OverridingStatic1 extends p1 {
+//	public static void goHome() {
+//		System.out.println("By train");
+//	}
+//
+//	public static void main(String[] args) {
+//		p1 obj = new OverridingStatic1();
+//		obj.goHome();
+//
+//	}
+//
+//}
