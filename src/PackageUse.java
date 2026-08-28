5ff1209194a764238cceefef57259f3977fd4927
Srashti3103 <srashtipatel31@gmail.com>
2026-08-28 23:25:38 +0530

    package use java

---
+++ src/PackageUse.java
@@ -0,0 +1,14 @@
+//import java.util.Date;
+import static java.lang.Math.*;
+public class PackageUse {
+
+	public static void main(String[] args) {
+		System.out.println("Today = "+ new java.util.Date());
+		
+		System.out.println(random());
+		System.out.println(sqrt(25));
+		System.out.println(ceil(3.4));
+
+	}
+
+}
