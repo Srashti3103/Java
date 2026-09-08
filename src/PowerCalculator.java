73022f4db3fd3a9c2bcaddaba6392bcf8d83fd0f
Srashti3103 <srashtipatel31@gmail.com>
2026-09-08 22:19:36 +0530

    power calculation

---
+++ src/PowerCalculator.java
@@ -0,0 +1,15 @@
+
+public class PowerCalculator {
+
+	public static void main(String[] args) {
+		int x, n, p=1;
+		x=5;
+		n=2;
+		for(int i=1;i<=n;i++) {
+			p=p*x;
+		}
+		System.out.println("Result(x^n) ="+p);
+
+	}
+
+}
