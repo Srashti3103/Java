3969b74e9d0097350234dd232cfd552f2500459a
Srashti3103 <srashtipatel31@gmail.com>
2026-09-10 20:14:54 +0530

    single inheritance

---
+++ src/SingleInheritance1.java
@@ -0,0 +1,14 @@
+//Example of Inheritance in java
+//Single inheritance ---->>>> 
+//A subclass inherit from one superclass
+
+public class SingleInheritance1 {
+
+	public static void main(String[] args) {
+		Dog myDog = new Dog();
+		myDog.eat();//Inherited method from Animal 
+		myDog.bark();//Method from Dog 
+
+	}
+
+}
