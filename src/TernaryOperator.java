22523da1037fc21bb60545932a6927b5e4d6b922
Srashti3103 <srashtipatel31@gmail.com>
2026-09-11 20:08:47 +0530

    ternary operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,17 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		/*
+		 * A shortland for  if-else statement 
+		 * operator : ? :
+		 * 
+		 * Syntax :
+		 *  condition ? expression1 : expression2
+		 */
+		int age =18;
+		String status = (age >= 18)?"Adult":"Minor";
+		System.out.println(status);//Adult
+	}
+
+}
