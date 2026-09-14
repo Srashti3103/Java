9827161d325b58a9b082243bd9bcfeedcde9b715
Srashti3103 <srashtipatel31@gmail.com>
2026-09-14 20:27:06 +0530

    search element in rotated sorted array

---
+++ src/SearchElementInRotatedSortedArray.java
@@ -0,0 +1,11 @@
+//import java.util.Arrays;
+//public class SearchElementInRotatedSortedArray {
+//
+//	public static void main(String[] args) {
+//		int [] arr = {4,5,6,7,1,2,3};
+//		
+//		int index = Arrays.binarySearch(arr, 2);
+//		System.out.println(index);
+//	}
+//
+//}
