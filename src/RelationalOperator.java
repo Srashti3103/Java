0fb2dead6700a2fed3b0f30fc492c90704b0df0f
Srashti3103 <srashtipatel31@gmail.com>
2026-09-16 11:32:55 +0530

    relational operator

---
+++ src/RelationalOperator.java
@@ -0,0 +1,17 @@
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+//		String x="abc";
+//		String y="abcog";
+		int x=10;
+		int y=20;
+		System.out.println(x == y); // Equal to: false
+		System.out.println(x != y); // Not equal to: true
+		System.out.println(x > y); // Greater than: false
+		System.out.println(x < y); // Less than: true
+		System.out.println(x >= y); // Greater than or equal to: false
+		System.out.println(x <= y); // Less than or equal to: true
+	}
+
+}
