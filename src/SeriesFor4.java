70eaa69bfaa8f8466f8b645fb8af6d7c405d4cbc
Srashti3103 <srashtipatel31@gmail.com>
2026-09-20 11:52:52 +0530

    series for

---
+++ src/SeriesFor4.java
@@ -0,0 +1,18 @@
+
+public class SeriesFor4 {
+
+	public static void main(String[] args) {
+		int x = 2, n = 3;
+		double sum = 1;
+		System.out.println("Enter base (x) : " + x);
+		System.out.println("Enter exponent limit (n): " + n);
+		System.out.print("Series : 1");
+		for (int i = 1; i <= n; i++) {
+			sum = sum + Math.pow(x, i) / 2 * i;
+			System.out.print("+(" + x + "^" + i + ")/" + (2 * i));
+		}
+		System.out.println();
+		System.out.println("Sum of Series = " + sum);
+	}
+
+}
