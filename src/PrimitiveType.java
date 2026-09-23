8dbaa9ae181619d6a848e3d6a34bd8e9961c1ba3
Srashti3103 <srashtipatel31@gmail.com>
2026-09-23 11:26:23 +0530

    primitive type

---
+++ src/PrimitiveType.java
@@ -0,0 +1,24 @@
+
+public class PrimitiveType {
+
+	public static void main(String[] args) {
+		byte b = 100;
+		short s = 30000;
+		int i = 2000000000;
+		long l= 9000000000000000000L;
+		float f = 3.14f;
+		double d= 123456.789;
+		char c = 'j';
+		boolean bool = true;
+		System.out.println("byte:"+b);
+		System.out.println("short:"+s);
+		System.out.println("int:"+i);
+		System.out.println("long:"+l);
+		System.out.println("float:"+f);
+		System.out.println("double:"+d);
+		System.out.println("char:"+c);
+		System.out.println("boolean:"+bool);
+
+	}
+
+}
