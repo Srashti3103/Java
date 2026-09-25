71506e318df44fc5434804910dfccf93bff94edc
Srashti3103 <srashtipatel31@gmail.com>
2026-09-25 19:58:14 +0530

    rotate array by one position

---
+++ src/advance_java/RotateArrayLeftByKPosition.java
@@ -0,0 +1,30 @@
+package advance_java;
+
+import java.util.Arrays;
+
+public class RotateArrayLeftByKPosition {
+	static void rotate(int arr[], int k) {
+		int n = arr.length;
+		k = k % n;
+		reverse(arr, 0, k - 1);
+		reverse(arr, k, n - 1);
+		reverse(arr, 0, n - 1);
+	}
+
+	static void reverse(int arr[], int start, int end) {
+		while (start < end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5 };
+		int k = 8;
+		rotate(arr, k);
+		System.out.println(Arrays.toString(arr));
+	}
+}
