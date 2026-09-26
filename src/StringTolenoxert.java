1eb44fc8517e71a3329190ffc4c55a3c19cb7a4e
Srashti3103 <srashtipatel31@gmail.com>
2026-09-26 11:24:48 +0530

    String tolerenoxet

---
+++ src/StringTolenoxert.java
@@ -0,0 +1,41 @@
+
+public class StringTolenoxert {
+
+	public static void main(String[] args) {
+
+	
+
+		System.out.println(Math.sqrt(25));
+
+		System.out.println(Math.floor(3.8));
+
+		System.out.println(Math.ceil(5.2));
+
+		System.out.println((int) (Math.random() * 100));
+
+	}
+
+}
+// System.out.println()
+/*
+ * System---->class out---->object--->System class ka nahi hai
+ * println()---->method()
+ * 
+ * System.--->access static contents System.out.println();
+ * 
+ * 
+ * class System---->console op { static PrintStream out;
+ * 
+ * }
+ * 
+ * 
+ * class PrintStream{--->print
+ * 
+ * println(){} println()int i{} println(float a){} println()String s{}
+ * 
+ * 
+ * }
+ * 
+ * 
+ * 
+ */
